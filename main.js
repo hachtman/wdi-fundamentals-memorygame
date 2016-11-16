5534ce400b536d860ea8d069c174df3c2795fed6
Hachtman <jackfllr1@googlemail.com>
2016-11-16 14:12:45 +0000

    DOM manipulation to create cards.

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "queen";
 
-if (cardTwo === cardFour)
+/*if (cardTwo === cardFour)
 {
 	alert("Sorry, try again!");
 }
@@ -19,10 +19,22 @@ else if(cardThree === cardFour)
 {
 	alert("You found a match!");
 }
-
 else 
 {
 	alert("Sorry, try again!");
-}
+} */
+
+var createCards = function() {
+	var gameBoard = document.getElementById('game-board');
+
+	for(var int = 0; int < 4; int++) {
+		var newCard = document.createElement('div')
+		newCard.className = 'card';
+		gameBoard.appendChild(newCard);
+	};
+};
+
+
+
 
 
